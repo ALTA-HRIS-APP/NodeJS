5a3cfc7699cc4a95237024a5f2db8cf07689aa96
Rajih Kharissuha <rajhk704@gmail.com>
2023-09-13 13:08:50 +0700

    set swagger

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 const port = 3000;
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get("/", (req, res) => {
   res.send("Selamat datang di Express!");
