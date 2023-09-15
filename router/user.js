49bed88a7d2418ba4a57b96a2c47099dfed2e14b
Rajihkharissuha12 <rajhk704@gmail.com>
2023-09-15 13:27:13 -0700

    add Fix: add empploye

--- router/user.js
+++ router/user.js
@@ -6,10 +6,13 @@ const {
   getUserbyId,
 } = require("../controller/user");
 const verifyToken = require("../middleware/verifyToken");
-const { loginValidation } = require("../middleware/user");
+const {
+  loginValidation,
+  addEmployeeValidation,
+} = require("../middleware/user");
 const router = express.Router();
 
-router.post(`/user`, addEmployee);
+router.post(`/user`, addEmployeeValidation, addEmployee);
 router.post("/login", loginValidation, login);
 router.get("/user", getAll);
 router.get("/profile", verifyToken, getUserbyId);
